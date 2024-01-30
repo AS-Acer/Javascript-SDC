77383cbf4cf0c01bb516f70d299cd5ebacc7164b
Asacer <148333712+AS-Acer@users.noreply.github.com>
2024-01-30 14:09:38 +0300

    Daily Progress

---
+++ mathMethods/app.js
@@ -0,0 +1,53 @@
+//math methods
+
+/*
+floor
+ceil
+round
+
+max 
+min
+
+random
+abs
+sqrt
+pow
+
+pi number
+
+*/
+
+
+let a =3.1515;
+let b = -15
+
+console.log(Math.floor(a));
+// result = 3
+
+console.log(Math.ceil(a));
+//result = 4
+
+console.log(Math.round(a));
+// +3,5---> 4 
+//result 3
+
+console.log(Math.max(1,2,3,4,545));
+//result 545
+
+console.log(Math.mib(1,2,3,4,545));
+//result 1
+
+console.log(Math.abs(b));
+//result 15
+
+console.log(Math.sqrt(36));
+//result 6
+
+console.log(Math.pow(5,2));
+//result 25
+
+console.log(Math.PI);
+//result 3.14
+
+console.log(Math.floor(Math.random()*100));
+//result random number
---
+++ stringMethods/app.js
@@ -0,0 +1,76 @@
+
+
+let header = "Modern web development course ";
+let header2= "       Modern web development   course         ";
+let date = 2024;
+
+//charAt()
+//concat()
+//indexof()
+//lastindexof()
+//toUpperCase()
+//toLowerCase()
+//trim()
+//slice()
+//substring()
+//replace()
+//split()
+//valueof()
+//startsWith()
+//endswith()
+
+//charAt()
+// let element = header.charAt(1);
+// console.log(element);
+
+
+//concat()
+// let result = header.concat(" ", date);
+// console.log(result);
+
+
+//indexof()
+// let index = header.indexOf("o");
+// console.log(index);
+
+//lastindexof
+// let index =header.lastIndexOf("course");
+// console.log(index);
+
+
+//trim()
+// console.log(header2.trim());
+
+
+//slice()
+// console.log(header);
+// console.log(header.slice(0,6));
+
+
+//substring()
+// console.log(header);
+// console.log(header.slice(0,6));
+
+//replace()
+// console.log(header);
+// console.log(header.replace("Course", "Camp"));
+
+ 
+//split()
+// console.log(header);
+// let splited = header.split(" ");
+// console.log(splited);
+
+
+//valueof()
+//console.log(header.valueOf()); 
+
+//startswith()
+// console.log(header);
+// console.log(header.startsWith("m"));
+
+//endsWith()
+// console.log(header);
+// console.log(header.endsWith("course"));
+
+
