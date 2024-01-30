35d7bc01f072c5c5bafe31e3ad1eaf8c415fbe88
Asacer <148333712+AS-Acer@users.noreply.github.com>
2024-01-30 19:25:02 +0300

    Daily Progress

---
+++ dateMethods/app.js
@@ -0,0 +1,24 @@
+//Date methods
+
+let moment = new Date(2003,8,23,6,45);
+console.log(Date);
+
+
+// console.log(moment.getHours());
+// console.log(moment.getTime());
+// console.log(moment.getDate());
+// console.log(moment.getDay());
+// console.log(moment.getMilliseconds());
+// console.log(moment.getMonth());//starts form 0
+// console.log(moment.toLocaleDateString());
+// console.log(moment.toLocaleTimeString());
+// console.log(moment.toLocaleString());
+
+
+// //set methods
+// console.log(moment.setDate(25));
+// console.log(moment.setHours(15));
+// console.log(moment.setMonth(8));
+// console.log(moment.setHours(moment.getHours()+2));
+
+
