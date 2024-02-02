ade7eb8e013761dd27bb3460f7a65a75104c42f3
Asacer <148333712+AS-Acer@users.noreply.github.com>
2024-02-02 12:30:41 +0300

    Daily Progress

--- localStorage/app.js
+++ localStorage/app.js
@@ -4,16 +4,16 @@
 console.log(window);
 
 //To add value
-sessionStorage.setItem("1453","Fatih");
-sessionStorage.setItem("2003","ASacer");
-sessionStorage.setItem(546,6846);//save as string string value
+localStorage.setItem("1453","Fatih");
+localStorage.setItem("2003","ASacer");
+localStorage.setItem(546,6846);//save as string string value
 
 //To remove item
-sessionStorage.removeItem("2003");
+localStorage.removeItem("2003");
 
 
 //To find item's value
-let value = sessionStorage.getItem("1453");
+let value = localStorage.getItem("1453");
 if (value==null) {
     console.log("Value not found");
 } else {
@@ -22,13 +22,13 @@ if (value==null) {
 
 
 //to remove all
-sessionStorage.clear();
+localStorage.clear();
 
 //to add array
 let names = ["Ahmet","Enes","Nuri","Said"];
-sessionStorage.setItem("names",JSON.stringify(names));
+localStorage.setItem("names",JSON.stringify(names));
 
-let namesValue = JSON.parse(sessionStorage.getItem("names"));
+let namesValue = JSON.parse(localStorage.getItem("names"));
 console.log(typeof namesValue);
 namesValue.forEach(function(name) {
     console.log(name);
